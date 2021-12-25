865e710c0c074e9c02ea84e7277aa208059a62c1
citixenken <kmuyesu@gmail.com>
2021-12-25 22:21:02 +0300

    first commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ken Muyesu on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
