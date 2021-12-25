2103bf79277dc4bafb2551cd75795a27b63c62be
citixenken <kmuyesu@gmail.com>
2021-12-25 22:40:12 +0300

    creating implicit animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationsAmount = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap here!") {
+            //increases button's animation by one everytime its tapped
+            animationsAmount += 0.5
+        }
+        .padding(50)
+        .background(.mint)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationsAmount)
+        .blur(radius: (animationsAmount - 1) * 3)
+        
+        //smooth transition
+        .animation(.default, value: animationsAmount)
     }
 }
 
