9c61da7870764a4943fe22fbe2a7e506b7994b3f
citixenken <kmuyesu@gmail.com>
2021-12-25 23:32:15 +0300

    customizing animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,22 +13,40 @@ struct ContentView: View {
     var body: some View {
         Button("Tap here!") {
             //increases button's animation by one everytime its tapped
-            animationsAmount += 0.5
+            //animationsAmount += 0.5
         }
         .padding(50)
         .background(.mint)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationsAmount)
-        .blur(radius: (animationsAmount - 1) * 3)
+        //        .scaleEffect(animationsAmount)
+        //        .blur(radius: (animationsAmount - 1) * 3)
         
         //smooth transition
-        .animation(.default, value: animationsAmount)
+        //.animation(.default, value: animationsAmount)
+        //.animation(.interpolatingSpring(stiffness: 80, damping: 1), value: animationsAmount)
+        //.animation(.easeInOut(duration: 2).delay(1), value: animationsAmount)
+        //.animation(.easeInOut(duration: 2).repeatCount(3, autoreverses: true), value: animationsAmount)
+        
+        .overlay(Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationsAmount)
+                    .opacity(2 - animationsAmount)
+                    .animation(
+                        .easeInOut(duration: 1)
+                            .repeatForever(autoreverses: false),
+                        value: animationsAmount
+                    )
+        )
+        .onAppear {
+            animationsAmount = 2
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
