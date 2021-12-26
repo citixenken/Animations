1f1b70da38dff320a2e96f690cde674c6ca9a220
citixenken <kmuyesu@gmail.com>
2021-12-26 13:28:46 +0300

    showing and hiding views with transitions

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let letters = Array("Hello, @citixenken")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+//    let letters = Array("Hello, @citixenken")
+//    @State private var enabled = false
+//    @State private var dragAmount = CGSize.zero
+    
+    @State private var isShowingRed = false
     
     var body: some View {
         
@@ -34,23 +36,39 @@ struct ContentView: View {
         //            }
         //            )
         
-        HStack(spacing: 0){
-            ForEach(0..<letters.count) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+//        HStack(spacing: 0){
+//            ForEach(0..<letters.count) { num in
+//                Text(String(letters[num]))
+//                    .padding(5)
+//                    .font(.title)
+//                    .background(enabled ? .blue : .red)
+//                    .offset(dragAmount)
+//                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+//            }
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { dragAmount = $0.translation }
+//                    .onEnded { _ in
+//                        dragAmount = .zero
+//                        enabled.toggle()
+//                    }
+//            )
+//        }
+        
+        VStack {
+            Button("Tap here!") {
+                withAnimation (.spring()){
+                    isShowingRed.toggle()
+                }
             }
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation }
-                    .onEnded { _ in
-                        dragAmount = .zero
-                        enabled.toggle()
-                    }
-            )
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 200, height: 200)
+                    //.transition(.scale)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
         }
     }
 }
